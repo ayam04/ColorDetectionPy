6e1c73075f157b1f3e2849a51c2523f2dade075f
Ayam <121638128+ayam04@users.noreply.github.com>
2023-06-06 01:49:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,118 +1,67 @@
-# Python code for Multiple Color Detection
-
-
-import numpy as np
 import cv2
+import pandas as pd
+
+img_path = r'C:\Users\ayamu\python programs\ColorDetectionPy\sample.jpg'
+img = cv2.imread(img_path)
+
+# declaring global variables (are used later on)
+clicked = False
+r = g = b = x_pos = y_pos = 0
+
+# Reading csv file with pandas and giving names to each column
+index = ["color", "color_name", "hex", "R", "G", "B"]
+csv = pd.read_csv('ColorDetectionPy\colors.csv', names=index, header=None)
 
 
-# Capturing video through webcam
-webcam = cv2.VideoCapture(0)
+# function to calculate minimum distance from all colors and get the most matching color
+def get_color_name(R, G, B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
+        if d <= minimum:
+            minimum = d
+            cname = csv.loc[i, "color_name"]
+    return cname
 
-# Start a while loop
-while(1):
-	
-	# Reading the video from the
-	# webcam in image frames
-	_, imageFrame = webcam.read()
 
-	# Convert the imageFrame in
-	# BGR(RGB color space) to
-	# HSV(hue-saturation-value)
-	# color space
-	hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
+# function to get x,y coordinates of mouse double click
+def draw_function(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b, g, r, x_pos, y_pos, clicked
+        clicked = True
+        x_pos = x
+        y_pos = y
+        b, g, r = img[y, x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
 
-	# Set range for red color and
-	# define mask
-	red_lower = np.array([136, 87, 111], np.uint8)
-	red_upper = np.array([180, 255, 255], np.uint8)
-	red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
 
-	# Set range for green color and
-	# define mask
-	green_lower = np.array([25, 52, 72], np.uint8)
-	green_upper = np.array([102, 255, 255], np.uint8)
-	green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', draw_function)
 
-	# Set range for blue color and
-	# define mask
-	blue_lower = np.array([94, 80, 2], np.uint8)
-	blue_upper = np.array([120, 255, 255], np.uint8)
-	blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
-	
-	# Morphological Transform, Dilation
-	# for each color and bitwise_and operator
-	# between imageFrame and mask determines
-	# to detect only that particular color
-	kernel = np.ones((5, 5), "uint8")
-	
-	# For red color
-	red_mask = cv2.dilate(red_mask, kernel)
-	res_red = cv2.bitwise_and(imageFrame, imageFrame,
-							mask = red_mask)
-	
-	# For green color
-	green_mask = cv2.dilate(green_mask, kernel)
-	res_green = cv2.bitwise_and(imageFrame, imageFrame,
-								mask = green_mask)
-	
-	# For blue color
-	blue_mask = cv2.dilate(blue_mask, kernel)
-	res_blue = cv2.bitwise_and(imageFrame, imageFrame,
-							mask = blue_mask)
+while True:
 
-	# Creating contour to track red color
-	contours, hierarchy = cv2.findContours(red_mask,
-										cv2.RETR_TREE,
-										cv2.CHAIN_APPROX_SIMPLE)
-	
-	for pic, contour in enumerate(contours):
-		area = cv2.contourArea(contour)
-		if(area > 300):
-			x, y, w, h = cv2.boundingRect(contour)
-			imageFrame = cv2.rectangle(imageFrame, (x, y),
-									(x + w, y + h),
-									(0, 0, 255), 2)
-			
-			cv2.putText(imageFrame, "Red Colour", (x, y),
-						cv2.FONT_HERSHEY_SIMPLEX, 1.0,
-						(0, 0, 255))	
+    cv2.imshow("image", img)
+    if clicked:
 
-	# Creating contour to track green color
-	contours, hierarchy = cv2.findContours(green_mask,
-										cv2.RETR_TREE,
-										cv2.CHAIN_APPROX_SIMPLE)
-	
-	for pic, contour in enumerate(contours):
-		area = cv2.contourArea(contour)
-		if(area > 300):
-			x, y, w, h = cv2.boundingRect(contour)
-			imageFrame = cv2.rectangle(imageFrame, (x, y),
-									(x + w, y + h),
-									(0, 255, 0), 2)
-			
-			cv2.putText(imageFrame, "Green Colour", (x, y),
-						cv2.FONT_HERSHEY_SIMPLEX,
-						1.0, (0, 255, 0))
+        # cv2.rectangle(image, start point, endpoint, color, thickness)-1 fills entire rectangle
+        cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
 
-	# Creating contour to track blue color
-	contours, hierarchy = cv2.findContours(blue_mask,
-										cv2.RETR_TREE,
-										cv2.CHAIN_APPROX_SIMPLE)
-	for pic, contour in enumerate(contours):
-		area = cv2.contourArea(contour)
-		if(area > 300):
-			x, y, w, h = cv2.boundingRect(contour)
-			imageFrame = cv2.rectangle(imageFrame, (x, y),
-									(x + w, y + h),
-									(255, 0, 0), 2)
-			
-			cv2.putText(imageFrame, "Blue Colour", (x, y),
-						cv2.FONT_HERSHEY_SIMPLEX,
-						1.0, (255, 0, 0))
-			
-	# Program Termination
-	cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame)
-	if cv2.waitKey(10) & 0xFF == ord('q'):
-		cap.release()
-		cv2.destroyAllWindows()
-		break
+        # Creating text string to display( Color name and RGB values )
+        text = get_color_name(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
+
+        # cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
+        cv2.putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # For very light colours we will display text in black colour
+        if r + g + b >= 600:
+            cv2.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
+
+        clicked = False
+
+    # Break the loop when user hits 'esc' key
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
